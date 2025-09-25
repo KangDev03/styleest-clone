3a00e9ffa50726c66f37b15df6c31a25ba4cfada
KangDev03 <khangdev.030204@gmail.com>
2025-09-25 14:47:31 +0700

    feat: fix animate on About section

--- src/features/home-page/landing-about.tsx
+++ src/features/home-page/landing-about.tsx
@@ -3,8 +3,11 @@ import { motion, useScroll, useTransform } from "motion/react"
 
 import SectionTitle from "@/components/ui/section-title";
 
+import { useBreakpoint } from "@/hooks/useBreakpoint";
+
 export default function About() {
   const containerRef = useRef(null);
+  const breakpoint = useBreakpoint();
 
   const { scrollYProgress } = useScroll({
     target: containerRef,
@@ -27,10 +30,19 @@ export default function About() {
     ["var(--color-muted)", "var(--color-primary)", "var(--color-primary)", "var(--color-primary)"]
   );
 
+  const noAnimateColor = "var(--color-primary)";
+
+  const getColor = (motionValue: any) => {
+    if (breakpoint === "lg" || breakpoint === "xl" || breakpoint === "2xl") {
+      return motionValue; // animate bình thường
+    }
+    return noAnimateColor; // không animate, màu cố định
+  };
+
   return (
     <section
       ref={containerRef}
-      className="h-[1734px] max-w-screen-xl mx-auto py-15 md:py-20 lg:py-30 px-6 md:px-15 lg:px-10"
+      className="lg:h-[1734px] max-w-screen-xl mx-auto py-15 md:py-20 lg:py-30 px-6 md:px-15 lg:px-10"
     >
       <div className="w-full sticky top-30">
         <div>
@@ -39,13 +51,13 @@ export default function About() {
 
         <div className="mt-6">
           <h1 className="text-center md:text-left lg:text-left md:text-primary lg:text-muted font-bold text-2xl md:text-3.5xl lg:text-5xl leading-8.5 md:leading-11 lg:leading-17">
-            <motion.span style={{ display: "block", color: line1Color }}>
-              We believe that style is a powerful form of self-expression. Our journey began with a simple yet
+            <motion.span style={{ display: "inline", color: getColor(line1Color) }}>
+              We believe that style is a powerful form of self-expression. Our journey began with a simple yet {" "}
             </motion.span>
-            <motion.span style={{ display: "block", color: line2Color }}>
-              profound vision – to curate a diverse collection of fashion-forward pieces that empower individuals
+            <motion.span style={{ display: "inline", color: getColor(line2Color) }}>
+              profound vision – to curate a diverse collection of fashion-forward pieces that empower individuals {" "}   
             </motion.span>
-            <motion.span style={{ display: "block", color: line3Color }}>
+            <motion.span style={{ display: "inline", color: getColor(line3Color) }}>
               to embrace their uniqueness.
             </motion.span>
           </h1>
